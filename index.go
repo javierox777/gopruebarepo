e833a45444501f35af21f95097baf4e52794f2e3
javierox777 <javier.allendes.cortez@gmail.com>
2023-07-04 15:35:30 -0400

    first commit

---
+++ index.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	modeluser "backend/model"
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://locahost:27018/gouser"))
+	if err != nil {
+		panic(err)
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		var users []modeluser.User
+		call := client.Database("gouser").Collection("user")
+		result, err := call.Find(context.TODO(), bson.M{})
+		if err != nil {
+			panic(err)
+		}
+
+		for result.Next(context.TODO()) {
+			var user modeluser.User
+			result.Decode(&user)
+			users = append(users, user)
+		}
+
+		return c.JSON(&fiber.Map{
+			"data": users,
+		})
+	})
+
+	app.Listen(":3000")
+}
---
+++ model/modeluser.go
@@ -0,0 +1,6 @@
+package modeluser
+
+type User struct {
+	Email    string `json : "email"`
+	password string `json : "password`
+}
